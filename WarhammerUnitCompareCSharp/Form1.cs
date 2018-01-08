c8a88d67d1f2760e50722dbf4d0f3bed1ab2204a
Willem Feijen <wfeijen@gmail.com>
2018-01-08 22:25:50 +0100

    Poging om de boel synchroon te krijgen

--- WarhammerUnitCompareCSharp/Form1.cs
+++ WarhammerUnitCompareCSharp/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WfeijenWH;
 
 namespace WarhammerUnitCompareCSharp
 {
@@ -15,6 +16,7 @@ namespace WarhammerUnitCompareCSharp
         public Form1()
         {
             InitializeComponent();
+            WeaponList weaponList=new WeaponList("");
         }
     }
 }
